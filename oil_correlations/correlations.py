9e9b47b578233dd0377d4926ffd89c110c87257c
Ibrahimshaker1 <ibrahimshaker829@gmail.com>
2024-06-25 02:35:58 -0800

    edit#3

--- oil_correlations/correlations.py
+++ oil_correlations/correlations.py
@@ -100,3 +100,123 @@ class Glaso:
             return glaso_rs_calculator(gg=self.gas_gravity, oil_api=self.oil_api, t=self.temp, p=self.pressure)
 
 
+    def bubble_point_pressure(self, rs):
+        """
+                    This function return the estimated value of Pb
+        """
+        def glaso_pb_calculator(rs, gg, t, oil_api):
+            t = t - 460  # temperature must be in F
+            x = (rs/gg)**0.816
+            y = t**0.172
+            z = oil_api**(-0.989)
+            pb_star = x * y * z
+            param_one = 1.7447*math.log10(pb_star)
+            param_two = 0.30218*(math.log10(pb_star))**2
+            log_pb = 1.7669 + param_one - param_two
+            pb = 10**log_pb
+            return pb
+        if isinstance(rs, list):
+            glaso_pb = {
+                "Rs": rs,
+                "pb": []
+            }
+            for r in rs:
+                glaso_pb["pb"].append(glaso_pb_calculator(rs=r, gg=self.gas_gravity, t=self.temp, oil_api=self.oil_api))
+            return glaso_pb
+        else:
+            return glaso_pb_calculator(rs=rs, gg=self.gas_gravity, t=self.temp, oil_api=self.oil_api)
+
+    def oil_formation_volume_factor(self, rs):
+        """
+            this function return the estimated value of Bo
+        """
+        oil_gravity = 141.5/(self.oil_api + 131.5)
+
+        def glaso_bo_calculator(rs, gg, og, t):
+            bob_star = rs*((gg/og)**0.526) + 0.968*(t - 460)
+            param_a = -6.58511 + 2.91329*math.log10(bob_star) - 0.27683*(math.log10(bob_star))**2
+            bo = 1 + 10**param_a
+            return bo
+        if isinstance(rs, list):
+            glaso_Bo = {
+                "Rs": rs,
+                "Bo": []
+            }
+            for r in rs:
+                glaso_Bo["Bo"].append(glaso_bo_calculator(rs=r, gg=self.gas_gravity, og=oil_gravity, t=self.temp))
+            return glaso_Bo
+        else:
+            return glaso_bo_calculator(rs=rs, gg=self.gas_gravity, og=oil_gravity, t=self.temp)
+
+
+class Marhoun:
+    def __init__(self, p: list | float, gg: float | int, oil_api: float | int, t: float | int):
+        self.pressure = p
+        self.gas_gravity = gg
+        self.oil_api = oil_api
+        self.temp = t
+
+    def solution_gas_oil_ratio(self):
+        """
+            this function return
+        """
+        oil_gravity = 141.5/(self.oil_api + 131.5)
+
+        def marhoun_rs_calculator(gg, og, t, p):
+            rs = (185.843208*(gg**1.877840)*(og**-3.1437)*(t**-1.32657)*p)**1.398441
+            return rs
+        if isinstance(self.pressure, list):
+            marhoun_rs = {
+                "P": self.pressure,
+                "Rs": []
+            }
+            for p in self.pressure:
+                marhoun_rs["Rs"].append(marhoun_rs_calculator(gg=self.gas_gravity, og=oil_gravity, t=self.temp, p=p))
+            return marhoun_rs
+        else:
+            return marhoun_rs_calculator(gg=self.gas_gravity, og=oil_gravity, t=self.temp, p=self.pressure)
+
+    def bubble_point_pressure(self, rs):
+        """
+            this function return estimated value of Pb
+        """
+        oil_gravity = 141.5 / (self.oil_api + 131.5)
+
+
+        def marhoun_pb_calculator(rs, gg, og, t):
+            pb = ((5.38088*10**-3)*(rs**0.715082)*(gg**-1.87784)*(og**3.1427)*(t**1.32657))
+            return pb
+        if isinstance(rs, list):
+            marhoun_pb ={
+                "Rs": rs,
+                "Pb": []
+            }
+            for r in rs:
+                marhoun_pb["Pb"].append(marhoun_pb_calculator(rs=r, gg=self.gas_gravity, og=oil_gravity, t=self.temp))
+            return marhoun_pb
+        else:
+            return marhoun_pb_calculator(rs=rs, gg=self.gas_gravity, og=oil_gravity, t=self.temp)
+
+
+    def oil_formation_volume_factor(self, rs):
+        """
+            this function return estimated value of Bo
+        """
+        oil_gravity = 141.5 / (self.oil_api + 131.5)
+        def marhoun_bo_calculator(rs, gg, og, t):
+            f = (rs**0.74239)*(gg**0.323294)*(og**-1.20204)
+            bo = 0.497069 + (0.862963*(10**-3)*t) + (0.182594*(10**-2)*f) + (0.318099*(10**-5)*(f**2))
+            return bo
+        if isinstance(rs, list):
+            marhoun_bo = {
+                "Rs": rs,
+                "Bo": []
+            }
+            for r in rs:
+                marhoun_bo["Bo"].append(marhoun_bo_calculator(rs=r, gg=self.gas_gravity, og=oil_gravity, t=self.temp))
+            return marhoun_bo
+        else:
+            return marhoun_bo_calculator(rs=rs, gg=self.gas_gravity, og=oil_gravity, t=self.temp)
+
+
+
--- oil_correlations/test.py
+++ oil_correlations/test.py
@@ -1,11 +1,19 @@
 from oil_correlations.module import read_match_data_file
-from correlations import Standing, Glaso
+from correlations import Standing, Glaso, Marhoun
 from pprint import pprint
 file = read_match_data_file("C:/Users/3D/Desktop/FYP-MY-DATA - Copy.csv")
-glaso = Glaso(p=2377, gg=0.851, t=(250 + 460), oil_api=47.7)
-rs = glaso.solution_gas_oil_ratio()
-print(rs)
-
+pressure = 4239
+gas = 0.848
+t = 180 + 460
+api = 27.3
+lab_rs = 807
+marhoun = Marhoun(p=[1000, 2000, 3000], gg=gas, oil_api=api, t=t)
+rs = marhoun.solution_gas_oil_ratio()
+pb = marhoun.bubble_point_pressure(rs=rs["Rs"])
+bo = marhoun.oil_formation_volume_factor(rs=rs["Rs"])
+print(f"Rs: {rs}")
+print(f"Pb: {pb}")
+print(f"Bo: {bo}")
 
 
 if __name__ == "__main__":
